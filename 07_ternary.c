20bf27137cf42104be75d078cb121666181c52d3
pranjulmittal <pranjulmittal210@gmail.com>
2026-09-21 23:11:59 +0530

    Delete 07_ternary.c

--- 07_ternary.c
+++
@@ -1,9 +0,0 @@
-#include<stdio.h>
-int main(){
-    int n ; 
-    printf("ente rthe number: ");
-    scanf("%d",&n);
-    // ternery operator condition - exp 1 ? exp 2 : exp 3
-    n%2==0 ? printf("even number") : printf("odd number");
-    return 0;
-}
